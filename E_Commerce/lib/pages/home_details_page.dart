b5d9c879685a7d7e9e679f28e4127e12c0b60127
Ehsan011 <ehsanhabibf@gmail.com>
2023-02-28 12:03:43 +0600

    last Update

--- E_Commerce/lib/pages/home_details_page.dart
+++ E_Commerce/lib/pages/home_details_page.dart
@@ -8,15 +8,34 @@ class HomeDetail extends StatelessWidget {
   const HomeDetail({Key? key, required this.catalog})
       : assert(catalog != null),
         super(key: key);
+
+
   @override
   Widget build(BuildContext context) {
+    final demoText="Firebase is a set of hosting services for any type of application. It offers NoSQL and real-time hosting of databases, content, social authentication, and notifications, or services, such as a real-time communication server";
     return Scaffold(
         backgroundColor: Mytheme.creamColor,
         appBar: AppBar(title: "Product Details".text.bold.make()),
         bottomNavigationBar: Container(
-          color: Colors.red,
-          child: ButtonBar(),
+          color: Colors.green,
+          child: ButtonBar(
+            alignment: MainAxisAlignment.spaceAround,
+             buttonPadding: Vx.mOnly(right: 16),
+            children: [
+              "Price \$ ${catalog.price}".text.xl3.white.make(),
+
+              ElevatedButton(
+                  onPressed: ()=>{
+
+                  },
+                style:ButtonStyle(shape: MaterialStatePropertyAll(StadiumBorder()),
+                ),
+                  child: "Add to Cart".text.bold.color(Colors.white).make(),
+              )
+            ],
+          ),
         ),
+
         body: SafeArea(
           bottom: false,
           child: Column(
@@ -35,10 +54,11 @@ class HomeDetail extends StatelessWidget {
                   width: context.screenWidth,
                   child: Column(
                     children: [
-                      25.heightBox,
+                      30.heightBox,
                       catalog.name.text.xl3.bold.make(),
-                      catalog.desc.text.xl2.bold.make(),
-                      20.heightBox,
+                      catalog.desc.text.xl.textStyle(context.captionStyle).make(),
+                      15.heightBox,
+                      demoText.text.make().p16(),
                     ],
                   ),
                 ),
